fac5f5630750f1d4222e77dd893bc3dbc31a6505
Alex Morega <alex@grep.ro>
2012-09-26 17:08:58 +0300

    rename ingest.py to data.py

--- manage.py
+++ manage.py
@@ -2,14 +2,14 @@
 
 import flask
 from flask.ext.script import Manager
-import ingest
+import data
 
 
 queries = flask.Blueprint('queries', __name__)
 
 @queries.route('/')
 def index():
-    record_list = ingest.Record.select().limit(10)
+    record_list = data.Record.select().limit(10)
     return flask.render_template('table.html', record_list=record_list)
 
 
@@ -17,13 +17,13 @@ def create_app():
     app = flask.Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('settings.py', silent=True)
     app.register_blueprint(queries)
-    ingest.DatabasePlugin().initialize(app)
+    data.DatabasePlugin().initialize(app)
     return app
 
 
 manager = Manager(create_app)
 
-ingest.register_commands(manager)
+data.register_commands(manager)
 
 
 if __name__ == '__main__':
