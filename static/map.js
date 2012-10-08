5061677a5507617fd474c6d99bd092b5dd2f669f
Alex Morega <alex@grep.ro>
2012-10-08 10:22:02 +0300

    get feature info

--- static/map.js
+++ static/map.js
@@ -11,6 +11,11 @@ App.map_point = function(lon, lat) {
 };
 
 
+App.show_feature_info = function() {
+    console.log('show_feature_info');
+};
+
+
 App.init = function() {
     var map = App.map = new OpenLayers.Map('map');
     map.addLayer(new OpenLayers.Layer.OSM());
@@ -30,6 +35,16 @@ App.init = function() {
          projection: App.webmerc,
          isBaseLayer: false});
     map.addLayer(money_layer);
+
+    var info_control = new OpenLayers.Control.WMSGetFeatureInfo({
+        url: App.MAPSERV_URL,
+        title: 'Identify features by clicking',
+        layers: [money_layer],
+        queryVisible: true
+    });
+    info_control.events.register("getfeatureinfo", App, App.show_feature_info);
+    map.addControl(info_control);
+    info_control.activate();
 
 };
 
