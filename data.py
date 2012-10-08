9409606e34a1b867cb561bf16958b2e09ee376c2
Alex Morega <alex@grep.ro>
2012-10-08 22:31:48 +0300

    save shell commands

--- data.py
+++ data.py
@@ -307,7 +307,8 @@ def register_commands(manager):
 
         print geojson.dumps(layer, indent=2)
 
-        # ogr2ogr -f sqlite -overwrite money.db money.geojson
+        # ./manage.py group_by_comuna < raw/plati_fega_feadr-2010.csv > maps/money_per_comuna.geojson
+        # ogr2ogr -f sqlite -overwrite maps/money_per_comuna.db maps/money_per_comuna.geojson
 
 
 def initialize(app):
